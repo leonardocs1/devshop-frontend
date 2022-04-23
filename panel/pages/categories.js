37b51e469421bc1140f1974bddefc8da59a92730
Leonardo Costa Santos <31082603+leonardocs1@users.noreply.github.com>
2022-04-23 09:55:31 -0300

    useQuery

--- panel/pages/categories.js
+++ panel/pages/categories.js
@@ -2,19 +2,7 @@ import React from 'react'
 import Card from '../components/Card'
 import Layout from '../components/Layout'
 import Title from '../components/Title'
-import useSWR from 'swr'
-
-const fetcher = async query => {
-  const res = await fetch('http://localhost:3000/graphql', {
-    headers: {
-      'Content-type': 'application/json'
-    },
-    method: 'POST',
-    body: query
-  })
-  const json = await res.json()
-  return json.data
-}
+import { useQuery } from '../lib/graphql'
 
 const query = {
   query: `
@@ -27,8 +15,7 @@ const query = {
 }
 
 const Index = () => {
-  const { data, error } = useSWR(JSON.stringify(query), fetcher)
-  console.log(data, error)
+  const { data, error } = useQuery(query)
   return (
     <Layout>
       <Title>Gerenciar categorias</Title>
