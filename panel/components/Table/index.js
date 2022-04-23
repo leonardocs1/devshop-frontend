237add23933f33ebed4cc47dacb3f9435a5cc42f
Leonardo Costa Santos <31082603+leonardocs1@users.noreply.github.com>
2022-04-23 11:19:54 -0300

    Listando categorias

--- panel/components/Table/index.js
+++ panel/components/Table/index.js
@@ -9,7 +9,7 @@ const TableHead = ({ children }) => (
 )
 
 const TableTh = ({ children }) => (
-  <th className='px-6 py-3 border-b border-gray-200 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider'>
+  <th className='px-6 py-3 border-b border-gray-200 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider'>
     {children}
   </th>
 )
