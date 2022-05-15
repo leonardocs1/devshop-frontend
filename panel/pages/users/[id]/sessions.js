2a994e0c386c0b4ce932d6c847db719ac9eb76ac
Leonardo Costa Santos <31082603+leonardocs1@users.noreply.github.com>
2022-05-15 10:20:05 -0300

    Ver detalhes das sessões ​

--- panel/pages/users/[id]/sessions.js
+++ panel/pages/users/[id]/sessions.js
@@ -5,8 +5,9 @@ import Title from '../../../components/Title'
 import { useMutation, useQuery } from '../../../lib/graphql'
 import Link from 'next/link'
 import { useRouter } from 'next/router'
-import Button from '../../../components/Button'
 import Alert from '../../../components/Alert'
+import { formatDistance, subDays } from 'date-fns'
+import { ptBR } from 'date-fns/locale'
 
 const DELETE_USER = `
 mutation deleteUser($id: String!) {
@@ -18,7 +19,7 @@ const Sessions = () => {
   const { data, mutate } = useQuery(`
   query {
     panelGetAllUserSessions(id: "${router.query.id}") {
-      id, userAgent
+      id, userAgent, lastUsedAt
     }
   }
   `)
@@ -31,16 +32,13 @@ const Sessions = () => {
     <Layout>
       <Title>Gerenciar sessões de usuários</Title>
       <div className='mt-8'></div>
-      <div>
-        <Button.Link href='/users/create'>Criar usuário</Button.Link>
-      </div>
       <div className='flex flex-col mt-8'>
         <div className='-my-2 py-2 overflow-x-auto sm:-mx-6 sm:px-6 lg:-mx-8 lg:px-8'>
           {data &&
             data.panelGetAllUserSessions &&
             data.panelGetAllUserSessions.length === 0 && (
               <Alert>
-                <p>Nenhum usuário criado até o momento.</p>
+                <p>Este usuário ainda não autenticou-se.</p>
               </Alert>
             )}
           {data &&
@@ -49,7 +47,8 @@ const Sessions = () => {
               <div className='align-middle inline-block min-w-full shadow overflow-hidden sm:rounded-lg border-b border-gray-200'>
                 <Table>
                   <Table.Head>
-                    <Table.Th>Usuários</Table.Th>
+                    <Table.Th>Sessões</Table.Th>
+                    <Table.Th>Usado</Table.Th>
                     <Table.Th></Table.Th>
                   </Table.Head>
 
@@ -63,34 +62,24 @@ const Sessions = () => {
                               <div className='flex items-center'>
                                 <div>
                                   <div className='text-sm leading-5 font-medium text-gray-900'>
-                                    {item.userAgent}
+                                    {item.id}
                                   </div>
                                   <div className='text-sm leading-5 text-gray-500'>
-                                    {item.id}
+                                    {item.userAgent}
                                   </div>
                                 </div>
                               </div>
                             </Table.Td>
 
                             <Table.Td>
-                              <Link href={`/users/${item.id}/sessions`}>
-                                <a
-                                  href='#'
-                                  className='text-indigo-600 hover:text-indigo-900'
-                                >
-                                  Sessões
-                                </a>
-                              </Link>{' '}
-                              |{' '}
-                              <Link href={`/users/${item.id}/passwd`}>
-                                <a
-                                  href='#'
-                                  className='text-indigo-600 hover:text-indigo-900'
-                                >
-                                  Alterar senha
-                                </a>
-                              </Link>{' '}
-                              |{' '}
+                              {formatDistance(
+                                new Date(item.lastUsedAt),
+                                new Date(),
+                                { locale: ptBR }
+                              )}{' '}
+                              atrás
+                            </Table.Td>
+                            <Table.Td>
                               <Link href={`/users/${item.id}/edit`}>
                                 <a
                                   href='#'
@@ -98,16 +87,7 @@ const Sessions = () => {
                                 >
                                   Editar
                                 </a>
-                              </Link>{' '}
-                              |{' '}
-                              <a
-                                href='#'
-                                className='text-indigo-600 hover:text-indigo-900'
-                                onClick={remove(item.id)}
-                              >
-                                {' '}
-                                Remover
-                              </a>
+                              </Link>
                             </Table.Td>
                           </Table.Tr>
                         )
